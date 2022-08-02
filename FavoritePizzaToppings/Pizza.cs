b7144bc86dc64bfeae2a30e41816c3d591c5f4fc
McKenzie Byrd <mckenziebyrd@me.com>
2022-08-02 18:17:28 -0500

    pulled down code from github

--- FavoritePizzaToppings/Pizza.cs
+++ FavoritePizzaToppings/Pizza.cs
@@ -8,6 +8,8 @@ namespace FavoritePizzaToppings
 {
     internal class Pizza
     {
+        //map - refers to the process of making a 1-to-1 relationship between object properties
         public List<string> Toppings { get; set; }
+        public string Size { get; set; }
     }
 }
--- FavoritePizzaToppings/Program.cs
+++ FavoritePizzaToppings/Program.cs
@@ -1,18 +1,27 @@
-﻿namespace FavoritePizzaToppings
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // read file into a string and deserialize JSON to a type
-            Movie movie1 = JsonConvert.DeserializeObject<Movie>(File.ReadAllText(@"c:\movie.json"));
+﻿using FavoritePizzaToppings;
+using Newtonsoft.Json;
 
-            // deserialize JSON directly from a file
-            using (StreamReader file = File.OpenText(@"c:\movie.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                Movie movie2 = (Movie)serializer.Deserialize(file, typeof(Movie));
-            }
-        }
-    }
+//A pizza chain wants to know which topping combinations are most popular for Build Your Own pizzas.
+
+//Given the sample of orders at [http://files.olo.com/pizzas.json](http://files.olo.com/pizzas.json), write an application (in C# or JavaScript)
+//to output the top 20 most frequently ordered pizza configurations, listing the toppings for each along with the number of times that pizza configuration has been
+//ordered.
+
+//Check this link [to read a JSON file] (https://www.newtonsoft.com/json/help/html/DeserializeWithJsonSerializerFromFile.htm) in c#.
+//You'll need to install the Newtonsoft.Json nuget package, and create a class that represents the pizzas.
+
+
+//one possible way to deserialize JSON
+// read file into a string and deserialize JSON to a type
+string result = File.ReadAllText("C:\\Users\\Neal\\source\\repos\\E18\\FavoritePizzaToppings\\FavoritePizzaToppings\\pizzas (1).json");
+var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(result);
+
+//another possible way
+// deserialize JSON directly from a file
+using (StreamReader file = File.OpenText(@"C:\Users\Neal\source\repos\E18\FavoritePizzaToppings\FavoritePizzaToppings\pizzas (1).json"))
+{
+    JsonSerializer serializer = new JsonSerializer();
+    var pizzas2 = serializer.Deserialize(file, typeof(List<Pizza>)) as List<Pizza>;
 }
+
+Console.ReadLine();
