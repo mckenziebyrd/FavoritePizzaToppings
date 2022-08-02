00727ed8ae9653446bf3ed249f6aa13f51034f66
McKenzie Byrd <mckenziebyrd@me.com>
2022-08-01 20:55:19 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FavoritePizzaToppings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read file into a string and deserialize JSON to a type
+            Movie movie1 = JsonConvert.DeserializeObject<Movie>(File.ReadAllText(@"c:\movie.json"));
+
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText(@"c:\movie.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Movie movie2 = (Movie)serializer.Deserialize(file, typeof(Movie));
+            }
+        }
+    }
+}
